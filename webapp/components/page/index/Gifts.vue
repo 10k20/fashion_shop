d781dca37c0d397cd67c51ce516feff6796607d3
10k20 <10k20.dev@gmail.com>
2020-11-06 16:12:11 +0300

    layout (not all) for gifts component

--- webapp/components/page/index/Gifts.vue
+++ webapp/components/page/index/Gifts.vue
@@ -1,5 +1,23 @@
 <template>
 <div class="wrapper">
+    <div class="title">
+      <div class="text">
+        <p>Подарки и распродажи на Монро24!</p>
+      </div>
+      <div class="line"></div>
+    </div>
+    <div class="banner">
+      <div class="title">
+          <p>Эксклюзивные купоны новичкам!</p>
+      </div>
+      <div class="card">
+        <div class="price">10 BYN</div>
+        <div class="button-wrapper"><a href="" class="button">Получить</a></div>
+      </div>
+      <div class="circs">
+        <a href="">Условия</a>
+      </div>
+    </div>
 </div>
 </template>
 
@@ -9,6 +27,59 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "~/assets/styles/breakpoints.scss";
+@import "~/assets/styles/colors.scss";
 
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 80rem;
+  margin: 0 auto;
+  padding-top: 4.25rem;
+
+  .title {
+    display: flex;
+    position: relative;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 2.5rem;
+
+    .text {
+      font-size: 1.5rem;
+      font-weight: 700;
+      width: 35%;
+      display: flex;
+      margin-bottom: 2.25rem;
+    }
+
+    .line {
+      height: 1px;
+      background-color: $dark;
+      width: 55%;
+      margin-right: 2rem;
+    }
+
+  }
+  .banner {
+      background-color: #000;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .title {
+          color: #fff;
+          font-size: 1.875rem;
+          margin: 2.5rem 0;
+      }
+      .card {
+          .price {
+              background-image: url();
+          }
+          .banner-wrapper {
+
+          }
+      }
+  }
+}
 </style>
