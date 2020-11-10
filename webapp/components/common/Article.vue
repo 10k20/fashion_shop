61d725ce2ab2ceb1a3fa3163c7d564593a1b2c01
10k20 <10k20.dev@gmail.com>
2020-11-10 20:40:04 +0300

    articles section layout

---
+++ webapp/components/common/Article.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="article">
+    <div class="article-title">
+      <p>{{ articleTitle }}</p>
+    </div>
+    <div class="article-text">
+      <pre>{{ articleText }}</pre>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    articleTitle: String,
+    articleText: String,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.article {
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  &-title {
+    font-style: 1.5rem;
+    font-weight: 700;
+    margin-bottom: 1.5rem;
+  }
+  &-text {
+    pre {
+      font-family: "Roboto", sans-serif;
+      max-width: 100%;
+    }
+  }
+}
+</style>
